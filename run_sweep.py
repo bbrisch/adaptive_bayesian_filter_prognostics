c22b29691be022e7d298eaf7a3e69a4b4cb9b0a7
bbrisch <benjaminbritoschiele@gmail.com>
2026-05-27 20:34:16 +0200

    file for running sweeps

--- run_sweep.py
+++ run_sweep.py
@@ -10,25 +10,14 @@ from optuna.visualization.matplotlib import plot_optimization_history
 from optuna.visualization.matplotlib import plot_parallel_coordinate
 from optuna.visualization.matplotlib import plot_param_importances
 
-
-with open(os.path.join('src','sweep_config.yml'), 'rb') as f:
+with open(os.path.join("src", "sweep_config.yml"), "rb") as f:
     config = yaml.safe_load(f)
 
-src.train_test_split(config['dataloader_config']['path_data'])
+# src.train_test_split(config['dataloader_config']['path_data'])
 
 
-dataloader, obs_std, popt  = src.initialize_objects(config['dataloader_config'])
+study = src.optimize_params(config)
+best_params = study.best_params
 
-study = src.optimize_params()
-
-# with open('study.pkl', 'wb') as f:
-#     pkl.dump(study,f )
-
-
-plot_optimization_history(study)
-plt.show()
-
-plot_parallel_coordinate(study)
-plt.show()
-plot_param_importances(study)
-plt.show()
+with open("study2.pkl", "wb") as f:
+    pkl.dump(study, f)
